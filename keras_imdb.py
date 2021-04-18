dba2ee764862e682cfb4a4983ddede2d3fbf5d6d
fmerizzi <merizzifabio@gmail.com>
2021-04-18 16:40:11 +0200

    90 %

--- keras_imdb.py
+++ keras_imdb.py
@@ -5,11 +5,11 @@ from keras import layers
 from keras.datasets import imdb
 import matplotlib.pyplot as plt
 
-(training_data, training_targets), (testing_data, testing_targets) = imdb.load_data(num_words=1000)
+(training_data, training_targets), (testing_data, testing_targets) = imdb.load_data(num_words=4000)
 data = np.concatenate((training_data, testing_data), axis=0)
 targets = np.concatenate((training_targets, testing_targets), axis=0)
 
-def vectorize(sequences, dimension = 1000):
+def vectorize(sequences, dimension = 4000):
  results = np.zeros((len(sequences), dimension))
  for i, sequence in enumerate(sequences):
   results[i, sequence] = 1
@@ -24,12 +24,12 @@ train_y = targets[10000:]
 
 model = models.Sequential()
 # Input - Layer
-model.add(layers.Dense(80, activation = "relu", input_shape=(1000, )))
+model.add(layers.Dense(100, activation = "relu", input_shape=(4000, )))
 # Hidden - Layers
-model.add(layers.Dropout(0.3, noise_shape=None, seed=None))
-model.add(layers.Dense(60, activation = "relu"))
-model.add(layers.Dropout(0.2, noise_shape=None, seed=None))
-model.add(layers.Dense(50, activation = "relu"))
+model.add(layers.Dropout(0.8, noise_shape=None, seed=None))
+model.add(layers.Dense(100, activation = "relu"))
+model.add(layers.Dropout(0.4, noise_shape=None, seed=None))
+model.add(layers.Dense(100, activation = "relu"))
 # Output- Layer
 model.add(layers.Dense(1, activation = "sigmoid"))
 model.summary()
@@ -41,8 +41,8 @@ model.compile(
 )
 results = model.fit(
  train_x, train_y,
- epochs= 5,
- batch_size = 50,
+ epochs= 8,
+ batch_size = 500,
  validation_data = (test_x, test_y)
 )
 
